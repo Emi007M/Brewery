40a394985b903029f478a2e0fe0c19d0e374ba94
JacobMiki <JacobMiki@users.noreply.github.com>
2017-07-01 15:58:44 +0200

    Cleanup

---
+++ Brewery/BreweryService/Controllers/BeerController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BreweryService.Models;
+using System.Reflection;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BreweryService.Controllers
+{
+    [Route("api/[controller]")]
+    public class BeerController : Controller
+    {
+
+        private readonly IBeerRepository _beerRepository;
+
+        public BeerController(IBeerRepository beerRepository)
+        {
+            _beerRepository = beerRepository;
+        }
+
+        //GET /api/beer
+        [HttpGet]
+        public IEnumerable<Beer> GetAll()
+        {
+            return _beerRepository.GetAll();
+        }
+
+        //GET /api/beer/{id}
+        [HttpGet("{id}", Name = "GetBeer")]
+        public IActionResult GetById(long id)
+        {
+            var item = _beerRepository.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        //POST /api/beer
+        [HttpPost]
+        public IActionResult Create([FromBody] Beer item)
+        {
+            if (item == null)
+            {
+                item = new Beer();
+                //return BadRequest();
+            }
+
+            _beerRepository.Add(item);
+
+            return CreatedAtRoute("GetBeer", new { id = item.Id }, item);
+        }
+
+        //PUT /api/beer/{id}, the whole obj, not only changes
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, [FromBody] Beer item)
+        {
+            if (item == null || item.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var beer = _beerRepository.Find(id);
+            if (beer == null)
+            {
+                return NotFound();
+            }
+
+            //TODO update only mentioned things
+            //beer.IsComplete = item.IsComplete;
+            //beer.Name = item.Name;
+            foreach (var prop in item.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
+            {
+                //if(prop.GetValue(item, null) != null)
+                prop.SetValue(beer,  prop.GetValue(item, null) );
+            }
+
+            _beerRepository.Update(beer);
+            return new NoContentResult();
+        }
+
+        //DELETE /api/beer/{id}
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var todo = _beerRepository.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _beerRepository.Remove(id);
+            return new NoContentResult();
+        }
+
+
+        //overall income/outcome statistics
+
+        //GET /api/beer/overall/
+        [HttpGet("overall")]
+        public OverallMoney GetOverallMoney()
+        {
+            return _beerRepository.GetOverallMoney();
+
+        }
+
+        //GET /api/beer/produce/
+        [HttpGet("produce")]
+        public OverallMoney ProduceAll()
+        {
+            _beerRepository.ProduceAll();
+            return _beerRepository.GetOverallMoney();
+
+        }
+    }
+}
---
+++ Brewery/BreweryService/Controllers/ClientController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BreweryService.Models;
+using System.Reflection;
+
+namespace BreweryService.Controllers
+{
+    [Route("api/[controller]")]
+    public class ClientController : Controller
+    {
+
+        private readonly IClientRepository _clientRepository;
+        private readonly IOrderRepository _orderRepository;
+
+        public ClientController(IClientRepository clientRepository, IOrderRepository orderRepository)
+        {
+            _clientRepository = clientRepository;
+            _orderRepository = orderRepository;
+        }
+
+        //POST /api/client
+        [HttpPost]
+        public IActionResult Create([FromBody] Client item)
+        {
+            if (item == null)
+            {
+                item = new Client("new client", "admin11");
+                //return BadRequest();
+            }
+
+            _clientRepository.Add(item);
+
+            return CreatedAtRoute("GetClient", new { id = item.Id }, item);
+        }
+
+        // GET api/client
+        [HttpGet]
+        public IEnumerable<Client> Get()
+        {
+            
+            return _clientRepository.GetAll();
+        }
+
+
+        //GET /api/client/{id}
+        [HttpGet("{id}", Name = "GetClient")]
+        public IActionResult GetById(long id)
+        {
+            var item = _clientRepository.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+
+        //GET /api/client/{id}/orders
+        [HttpGet("{id}/orders", Name = "GetOrders")]
+        public ClientOrders GetOrdersByClient(long id)
+        {
+            IEnumerable<Order> orders = new List<Order>(_orderRepository.GetAll().Where(o => o.ClientId == id));
+            return new ClientOrders(orders);
+
+
+        }
+        //GET /api/client/orders
+        [HttpGet("orders", Name = "GetAllOrders")]
+        public IEnumerable<Order> GetOrders()
+        {
+            IEnumerable<Order> orders = new List<Order>();
+            orders = _orderRepository.GetAll().OrderBy(o => o.Date);
+            return orders;
+            // return _orderRepository.GetAll();
+
+
+        }
+
+        //GET /api/client/orders/{id}
+        [HttpGet("orders/{id}", Name = "GetOrder")]
+        public Order GetOrder(long id)
+        {
+            return _orderRepository.Find(id);
+        }
+
+
+
+        //POST /api/client/{id}/info
+        [HttpPost("{id}/info")]
+        public IActionResult UpdateInfo(long id, [FromBody] List<ClientInfoFromShop> item)
+        {
+
+            if (item == null)
+            {
+                return Json(new { success = 0 });
+            }
+
+            _clientRepository.Find(id).Info = item;
+
+            return Json(new { success = 1 });
+        }
+
+        //POST /api/client/{id}/order
+        // dictionary: { "beerId": id, "amount": amount, "key": password }
+        [HttpPost("{id}/order")]
+        public IActionResult UpdateInfo(long id, [FromBody] Dictionary<string,string> item)
+        {
+            //validate password
+            if (_clientRepository.IsKeyClientValid(id, (string)item["key"]))
+            {
+                //check if beer id exists and amount of bottles is sufficient
+                long order_placed = 
+                    _orderRepository.AddOrder(int.Parse(item["beerId"]), id, int.Parse(item["amount"]));
+                if (!order_placed.Equals(-1))
+                    //return CreatedAtRoute("GetOrder", id = order_placed);
+                    return Json( GetOrder(order_placed) );
+                else
+                    return Json( new{ success = 0} );
+            }
+
+            else
+                return null;
+        }
+    }
+}
